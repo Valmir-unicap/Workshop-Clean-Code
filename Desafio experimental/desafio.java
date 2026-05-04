36c8a19f56ee650a3004d655bfaea38d5ab84c75
Valmir Júnior <61609240+Valmir-unicap@users.noreply.github.com>
2026-05-04 15:24:09 -0300

    Refactor input handling and update method signatures

--- Desafio experimental/desafio.java
+++ Desafio experimental/desafio.java
@@ -3,33 +3,48 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.println("Digite um número: ");
-        int i = in.nextInt(); //recebe double
-        if(i < 0){
-            i = in.nextInt();
-        }
-        System.out.println("Digite um nome: ");
+        Scanner in = new Scanner(System.in);//entrada de dados
+        System.out.print("num1: ");
+        int num1 = in.nextInt(); //recebe double
+        if(num1 < 0){//validação
+            num1 = in.nextInt();//espera receber numero maior que 0
+        } System.out.print("nome: ");
         in.nextLine();//Limpeza de buffer
         String n = in.nextLine();
-        f(n, i);
+        f(n, num1, 51,1.81);
+        double h  = t(1621,450);
+        System.out.println("soma do total salario + vale = "+h);
     }
-
-    public static void f(String n, int i) {
-        String x = "Olá, " + n + "!";
-        System.out.println(x);
+    
+    //Esse metodo faz calculo
+    public static void f(String n, int i, double p, double a) {
+        String x = "Olá, " + n + "!";//monta menssagem
+        double rs;
+        double k;
+        System.out.println(x);//exibi menssagem
         if (i < 18) {
-            System.out.println("Você é maior de idade.");
+            rs = p / (a*a);
+            k = Math.round(rs * 100.0) / 100.0;
+            System.out.println("Pode ter CNH");
+            System.out.println(k +" é a massa");
         } else {
-            System.out.println("Você é menor de idade.");
+            rs = p / (a*a);
+            k = Math.round(rs * 100.0) / 100.0;
+            System.out.println("Não pode ter CNH");
+            System.out.println(k +" é a massa");//exibi o valor da massa
         }
     }
-
-
-    public static double calcula(double p, double a){
-        double rs;
-        rs = p / (a*a);
-        return rs;
+    //esse retorna troco mercado
+    public static double t(double p, double a){
+        double s; s = p + a;//subtração
+        return s;//retona valor
     }
-
+    
+    public static void tab(int num1){
+        int i,r;
+        for (i=0; i<=10; i=i+1){//incrementa + 1
+            r = num1*i;//resultado
+            System.out.println(num1+" * "+i+" = "+r);//exibi
+        }
+    }
 }
